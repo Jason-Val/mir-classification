1857fa79cf22b7a11102d28fca2cbc065427f286
jason <jasonvallada@umass.edu>
2017-12-09 21:48:25 -0500

    added a logger for neural net test records

---
+++ logger.py
@@ -0,0 +1,22 @@
+import os
+
+def write_log(name, n_classes, epochs, batch_size, time, learning_rate, subset, train_accuracy, test_accuracy, cost, valid):
+    
+    filename = './logs/{}'.format(name)
+    os.makedirs(os.path.dirname(filename), exist_ok=True)
+    with open('{}.log'.format(filename), 'w') as f:
+        f.write('Number of classes: {}\n'.format(n_classes))
+        f.write('Elapsed time: {}\n'.format(time))
+        if batch_size <= 0:
+            f.write('One batch.\nNumber of epochs: {}\n'.format(epochs))
+        else:
+            f.write('Mini-batch size: {}\nNumber of epochs: {}\n'.format(batch_size, epochs))
+        f.write('Learning rate: {}\n'.format(learning_rate))
+        f.write('Subset: {}\n'.format(subset))
+        f.write('------------------------------------\n')
+        f.write('Testing accuracy: {}\n'.format(test_accuracy))
+        f.write('Training accuracy: {}\n'.format(train_accuracy))
+        f.write('------------------------------------\n'.format())
+        f.write('cost history,\tvalidation history\n'.format())
+        for c, v in zip(cost, valid):
+            f.write('{},\t{}\n'.format(c, v))
