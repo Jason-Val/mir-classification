5c5d547142dffbb0f1d591ff12efcf0d20640406
jason <jasonvallada@umass.edu>
2017-12-09 21:49:08 -0500

    add ply_export; exports pca output to a point cloud

---
+++ ply_export.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+def write_ply(vertices, name):
+    with open('./visualization/pca/ply/{}.ply'.format(name), 'w') as f:
+        f.write('ply\nformat ascii 1.0\n')
+        f.write('element vertex {}\n'.format(len(vertices)))
+        f.write('property float x\nproperty float y\nproperty float z\nend_header\n')
+        for vertex in vertices:
+            f.write('{} {} {}\n'.format(vertex[2], vertex[1], vertex[0]))        #pca1->z, pca2->y, pca3->x
