a3b846716b116e92fee3431960512d56c71e87de
jason <jasonvallada@umass.edu>
2017-12-14 17:47:12 -0500

    Cleaned up training

--- nn_train.py
+++ nn_train.py
@@ -74,45 +74,6 @@ def train_net(net, test_id, training_epochs, batch_size, minutes):
         train_accuracy = sess.run(accuracy, feed_dict={net.X:x_train, net.y_:y_train, net.keep_prob:1.0})
         print("training accuracy:")
         print(train_accuracy)
-        
-        """
-        y_pred = sess.run(tf.round(net.output), feed_dict={net.X: x_test, net.keep_prob:1})
-        y_true = sess.run(net.y_, feed_dict={net.y_: y_test, net.keep_prob:1})
-        
-        print(y_pred[1])
-        print(y_true[1])
-        for i in range(len(y_pred[5])):
-            if y_pred[5][i] != y_true[5][i]:
-                print("Index {}, {} != {}".format(i, y_pred[5][i], y_true[5][i]))
-        print('---------')
-
-        print(y_pred[56])
-        print(y_true[56])
-        for i in range(len(y_pred[50])):
-            if y_pred[50][i] != y_true[50][i]:
-                print("Index {}, {} != {}".format(i, y_pred[50][i], y_true[50][i]))
-        print('---------')
-        
-        print(y_pred[4])
-        print(y_true[4])
-        for i in range(len(y_pred[2])):
-            if y_pred[2][i] != y_true[2][i]:
-                print("Index {}, {} != {}".format(i, y_pred[2][i], y_true[2][i]))
-        print('---------')
-
-        print(y_pred[9])
-        print(y_true[9])
-        for i in range(len(y_pred[0])):
-            if y_pred[0][i] != y_true[0][i]:
-                print("Index {}, {} != {}".format(i, y_pred[0][i], y_true[0][i]))
-        
-        print('---------')
-        print(y_pred[150])
-        print(y_true[150])
-        for i in range(len(y_pred[57])):
-            if y_pred[57][i] != y_true[57][i]:
-                print("Index {}, {} != {}".format(i, y_pred[57][i], y_true[57][i]))
-        """
 
     return (test_accuracy, train_accuracy, cost_hist, val_hist, epochs, d2-d1)
 
@@ -124,26 +85,26 @@ def show_plot(cost_hist, val_hist):
 
 
 def go():
-    n_classes = 3
+    n_classes = 4
     subset='small'
     
-    #fma_pca.init_mel(n_classes, subset=subset, reuse=True, pca_on=True)
+    fma_pca.init_mel(n_classes, subset=subset, reuse=True, pca_on=False)
 
-    fma_pca.init_mel(n_classes, subset=subset, reuse=True, pca_on=True)
+    #fma_pca.init_mel(n_classes, subset=subset, reuse=True)
     
-    test_id = 'Presnetation/5min_'        # for the log file name
+    test_id = 'Accuracy/5min_'        # for the log file name
     training_epochs = 100000000
     batch_size = 400
-    minutes = 5
+    minutes = 60*2
     n_dim = fma_pca.n_dim()
     sd = 1 / np.sqrt(n_dim)
     learning_rate=.01
     
 ##############################################################################
     
-    test_id1 = '{}cnn_c4_pca_on_frequency_real'.format(test_id)
+    test_id1 = '{}cnn_c4_2hrs'.format(test_id)
     # 128, 57 if pca, else 128,128
-    net = cnn2.build_net([128, 57], n_classes, learning_rate, pca_on=True)
+    net = cnn2.build_net([128, 128], n_classes, learning_rate)
     
     test_accuracy, train_accuracy, cost_hist, val_hist, epochs, time = train_net(net, test_id1, training_epochs, batch_size, minutes)
     
@@ -152,71 +113,6 @@ def go():
     show_plot(cost_hist, val_hist)
     show_plot(cost_hist, [])
     show_plot(val_hist, [])
-
-##############################################################################
-    """
-    fma_pca.init(n_classes, subset=subset, reuse=False, pca_on=False)
-    
-    n_dim = fma_pca.n_dim()
-
-    test_id1 = '{}nn_c2_pca_off'.format(test_id)
-    # 128, 57 if pca, else 128,128
-    net = akshaynet.build_net(n_dim, n_classes, learning_rate)
-    
-    test_accuracy, train_accuracy, cost_hist, val_hist, epochs, time = train_net(net, test_id1, training_epochs, batch_size, minutes)
-    
-    logger.write_log(test_id1, n_classes, epochs, batch_size, time, learning_rate, subset, train_accuracy, test_accuracy, cost_hist, val_hist)
-    
-    show_plot(cost_hist, val_hist)
-    show_plot(cost_hist, [])
-    show_plot(val_hist, [])
-    """
-    
-##############################################################################
-    """
-    fma_pca.init_mel(n_classes, subset=subset, reuse=False, pca_on=False)
-    
-    test_id = 'Presnetation/5min_'        # for the log file name
-    training_epochs = 100000000
-    batch_size = 400
-    minutes = 5
-    n_dim = fma_pca.n_dim()
-    sd = 1 / np.sqrt(n_dim)
-    learning_rate=.01
-
-    subset='small'
-    #fma_pca.init_mel(n_classes, subset=subset, reuse=False, pca_on=False)
-"""
-##############################################################################
-    """
-    test_id1 = '{}cnn_c2_pca_off'.format(test_id)
-    # 128, 57 if pca, else 128,128
-    net = cnn2.build_net([128,128], n_classes, learning_rate)
-    
-    test_accuracy, train_accuracy, cost_hist, val_hist, epochs, time = train_net(net, test_id1, training_epochs, batch_size, minutes)
-    
-    logger.write_log(test_id1, n_classes, epochs, batch_size, time, learning_rate, subset, train_accuracy, test_accuracy, cost_hist, val_hist)
-    
-    show_plot(cost_hist, val_hist)
-    show_plot(cost_hist, [])
-    show_plot(val_hist, [])
-    """
-##############################################################################
-"""
-    #fma_pca.init(n_classes, subset=subset, reuse=False, pca_on=True)
-
-    test_id1 = '{}nn_c2_pca_off'.format(test_id)
-    # 128, 57 if pca, else 128,128
-    net = cnn2.build_net([128,128], n_classes, learning_rate, pca_on=False)
-    
-    test_accuracy, train_accuracy, cost_hist, val_hist, epochs, time = train_net(net, test_id1, training_epochs, batch_size, minutes)
-    
-    logger.write_log(test_id1, n_classes, epochs, batch_size, time, learning_rate, subset, train_accuracy, test_accuracy, cost_hist, val_hist)
-    
-    show_plot(cost_hist, val_hist)
-    show_plot(cost_hist, [])
-    show_plot(val_hist, [])
-"""
 
 if __name__ == '__main__':
     go()
