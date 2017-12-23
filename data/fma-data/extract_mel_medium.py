c44eaa11eb4d5a1672b9622787990c10ce68b886
jason <jasonvallada@umass.edu>
2017-12-23 18:24:44 -0500

    added comments

--- data/fma-data/extract_mel_medium.py
+++ data/fma-data/extract_mel_medium.py
@@ -2,8 +2,9 @@ import librosa
 import os
 import zipfile as zf
 import random
-import torchaudio #loads files faster than librosa
 import numpy as np
+# faster, but segfaults:
+# import torchaudio
 
 csv_str = ''
 
@@ -12,7 +13,7 @@ count = 0
 with zf.ZipFile('./fma_medium.zip') as fma_zip:
     contents = fma_zip.namelist()
     for track in contents:
-        if '.mp3' in track and '005574' not in track and '012171' not in track:
+        if '.mp3' in track:
             track_path = fma_zip.extract(track)
             
             count += 1
@@ -20,8 +21,11 @@ with zf.ZipFile('./fma_medium.zip') as fma_zip:
             print("Extracting track #{}, {}".format(count, track_path.split('/')[-1]), sep=' ', end='\r', flush=True)
             
             try:
-                #clip = torchaudio.load(track_path)[0].numpy()  # faster, but segfaults
+                # faster, but segfaults
+                """
+                #clip = torchaudio.load(track_path)[0].numpy()
                 #clip = librosa.to_mono(np.transpose(clip))
+                """
 
                 clip, _ = librosa.load(track_path)
                 
@@ -31,12 +35,10 @@ with zf.ZipFile('./fma_medium.zip') as fma_zip:
                 
                 if mel.shape[1] > 129:
                     # Write header - track id
-                    #print(track_path.split('.')[0].split('/')[-1])
                     csv_str += '{}'.format(int(track_path.split('.')[0].split('/')[-1]))
                     for x in range(127):
                         csv_str += ','
                     csv_str += '\n'
-                    #print("progress...")
                     # Write data for that track; 128 lines of 1024 elements
                     for freq in range(len(mel)):
                         r = random.randint(0, mel.shape[1]-128)
@@ -49,6 +51,5 @@ with zf.ZipFile('./fma_medium.zip') as fma_zip:
 
 os.remove('./fma_medium.zip')
 
-with zf.ZipFile('./fma_metadata/melspectrogram.zip', 'w') as mel_zip:
+with zf.ZipFile('./fma_metadata/melspectrogram_medium.zip', 'w') as mel_zip:
     mel_zip.writestr('melspectrogram.csv', csv_str)
-    
